c65ead7d0b696704a28ae4af7824f6d831e13d0f
Ozod26 <ozodfarid46@gmail.com>
2024-02-05 18:51:35 +0900

    BRR: Initial Setup

--- train.js
+++ train.js
@@ -23,15 +23,22 @@ app.set("view engine", "ejs");
 
 
 // 4 Routing code  
-app.get("/hello", function(req, res) {
- res.end(`<h1>HELLO WORLD</h1>`);
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "success" })
 });
-app.get("/gift", function(req, res) {
-    res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
-   });
-   app.get("/", function(req, res) {
-    res.render('project');
-   });
+
+app.get("/", function (req, res ) {
+    res.render("harid");
+});
+
+
+// app.get("/hello", function(req, res) {
+//  res.end(`<h1>HELLO WORLD</h1>`);
+// });
+// app.get("/gift", function(req, res) {
+//     res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
+//    });
 
 
 const server = http.createServer(app);
