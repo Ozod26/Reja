76d76926d5589b235f309d8c9ec8bb76bd68a8df
Ozod26 <ozodfarid46@gmail.com>
2024-02-05 14:34:48 +0900

    BRR: build  express wep server

---
+++ server.js
@@ -0,0 +1,38 @@
+console.log("Wep Serverni boshlash");
+const express = require("express");
+const app = express(); // () expressni app objectini yuboradi 
+const http = require("http");
+
+
+
+
+
+// 1: Kirish code
+//expressga kirib kelaytgan malumotlarga bogliq bolgan code yoziladi
+app.use(express.static("public"));
+app.use(express.json()); //objectga ogirib beradi
+app.use(express.urlencoded({extended: true})); // hrml, formdan post qilgan narsani qabul qd 
+
+// 2: session code
+
+// 3: Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+
+// 4 Routing code  
+app.get("/hello", function(req, res) {
+ res.end(`<h1>HELLO WORLD</h1>`);
+});
+app.get("/gift", function(req, res) {
+    res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
+   });
+
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function() {
+    console.log(`The server is running successfully on port: ${PORT} `);
+});
+
+// https://pixso.net
