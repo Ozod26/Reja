f0d1a0a2668c82d7d5ef65d20660c9eeb270a6b6
Ozod26 <ozodfarid46@gmail.com>
2024-02-06 02:24:12 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const express = require("express");
 const res = require('express/lib/response')
 const app = express(); // () expressni app objectini yuboradi 
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if(err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data)
+  }
+});
 
 
 
@@ -25,6 +35,10 @@ app.set("view engine", "ejs");
 app.post("/create-item", (req, res) => {
     console.log(req.body);
     res.json({test: "success" })
+});
+
+app.get('/author', (req, res) => {
+  res.render("author", {user: user}); 
 });
 
 app.get("/", function (req, res ) {
